80bdbc6eb495fd019ec89be9d6fb5a198730f6b9
carloslima <carloslima@brq.com>
2022-06-17 18:09:40 -0300

    refatoração koin

--- app/src/main/java/com/chslcompany/spaceflightnews/data/di/DataModule.kt
+++ app/src/main/java/com/chslcompany/spaceflightnews/data/di/DataModule.kt
@@ -8,6 +8,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -17,47 +18,39 @@ object DataModule {
 
     private const val URL_BASE = "https://api.spaceflightnewsapi.net/v3/"
 
-    fun load() = loadKoinModules(networkModule + repositoryModule())
+    fun load() = loadKoinModules(networkModule() + repositoryModule())
 
     private fun repositoryModule() = module {
         single<PostRepository> { PostRepositoryImpl(get()) }
     }
 
-    private val networkModule = module {
-        fun providerOkHttpClient(): OkHttpClient =
-            OkHttpClient.Builder().apply {
-                connectTimeout(60L, TimeUnit.SECONDS)
-                readTimeout(60L, TimeUnit.SECONDS)
-                writeTimeout(60L, TimeUnit.SECONDS)
-                    .addInterceptor(
-                        HttpLoggingInterceptor().setLevel(
-                            HttpLoggingInterceptor.Level.BODY
+    private fun networkModule(): Module {
+        return module {
+            single<SpaceFlightNewsService> { providerService(get(), get()) }
+            single { Moshi.Builder().add(KotlinJsonAdapterFactory()).build() }
+            single {
+                OkHttpClient.Builder().apply {
+                    connectTimeout(60L, TimeUnit.SECONDS)
+                    readTimeout(60L, TimeUnit.SECONDS)
+                    writeTimeout(60L, TimeUnit.SECONDS)
+                        .addInterceptor(
+                            HttpLoggingInterceptor().setLevel(
+                                HttpLoggingInterceptor.Level.BODY
+                            )
                         )
-                    )
-            }.build()
-
-
-        fun providerRetrofit(client: OkHttpClient, factory: Moshi): Retrofit =
-            Retrofit.Builder().apply {
-                baseUrl(URL_BASE)
-                addConverterFactory(MoshiConverterFactory.create(factory))
-                client(client)
-            }.build()
-
-
-        fun providerSpaceFlightService(retrofit: Retrofit): SpaceFlightNewsService =
-            retrofit.create(SpaceFlightNewsService::class.java)
-
-        single {
-            providerSpaceFlightService(
-                providerRetrofit(
-                    providerOkHttpClient(),
-                    get()
-                )
-            )
+                }.build()
+            }
         }
-        single { Moshi.Builder().add(KotlinJsonAdapterFactory()).build() }
     }
+
+    private inline fun <reified T> providerService(client: OkHttpClient, factory: Moshi): T =
+        Retrofit.Builder().apply {
+            baseUrl(URL_BASE)
+            addConverterFactory(MoshiConverterFactory.create(factory))
+            client(client)
+        }.build()
+            .create(T::class.java)
 
 
 }
+
