9be4c5d46b5099c27419f3c61fe18cd21d9d327a
carloslima <carloslima@brq.com>
2022-08-23 20:49:46 -0300

    configurando toolbar

--- app/src/main/java/com/chslcompany/spaceflightnews/ui/activity/MainActivity.kt
+++ app/src/main/java/com/chslcompany/spaceflightnews/ui/activity/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.chslcompany.spaceflightnews.R
 import com.chslcompany.spaceflightnews.databinding.ActivityMainBinding
+import android.view.MenuItem
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,14 +23,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.mainToolbar)
+
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.string.latest_news, R.string.latest_blogs, R.string.latest_reports
-            )
-        )
-        binding.mainToolbar.setupWithNavController(navController,appBarConfiguration)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
         binding.navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_news -> {
+                    supportActionBar?.title = getString(R.string.latest_news)
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                R.id.navigation_blogs -> {
+                    supportActionBar?.title = getString(R.string.latest_blogs)
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                R.id.navigation_report -> {
+                    supportActionBar?.title = getString(R.string.latest_reports)
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+                else -> {
+                    supportActionBar?.title = getString(R.string.details)
+                    supportActionBar?.setDisplayShowHomeEnabled(true)
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                }
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
