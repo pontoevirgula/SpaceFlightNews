4fcadcd52a4f7f34de23836c84c2785b7aef1f89
carloslima <carloslima@brq.com>
2022-09-28 00:17:30 -0300

    usando design pattern DTO nos testes unitarios

---
+++ app/src/test/java/com/chslcompany/PostDTOTest.kt
@@ -0,0 +1,40 @@
+package com.chslcompany
+
+import com.chslcompany.spaceflightnews.data.model.Post
+import com.chslcompany.spaceflightnews.data.network.LaunchDTO
+import com.chslcompany.spaceflightnews.data.network.PostDTO
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+
+@RunWith(JUnit4::class)
+class PostDTOTest {
+
+    private val launchDto = LaunchDTO(
+            id = "0d779392-1a36-4c1e-b0b8-ec11e3031ee6",
+            provider = "Launch Library 2"
+    )
+
+    private val postDto = PostDTO(
+        id = 12782,
+        title = "Crew-3 mission cleared for launch",
+        url = "https://spacenews.com/crew-3-mission-cleared-for-launch/",
+        imageUrl = "https://spacenews.com/wp-content/uploads/2021/11/crew2-chutes.jpg",
+        summary = "NASA and SpaceX are ready to proceed with the launch of a commercial crew mission Nov. 10 after overcoming weather and astronaut health issues as well as concerns about the spacecraft’s parachutes.",
+        publishedAt = "2021-11-10T09:27:02.000Z",
+        updatedAt = "2021-11-10T09:38:23.654Z",
+        launches = listOf(launchDto)
+    )
+
+    @Test
+    fun `should convert correctly to model entity`(){
+       val post : Post = postDto.toPostModel()
+
+        assertTrue(post is Post)
+        assertEquals(post.title, postDto.title)
+        assertTrue(post.launches.isNotEmpty())
+    }
+}
