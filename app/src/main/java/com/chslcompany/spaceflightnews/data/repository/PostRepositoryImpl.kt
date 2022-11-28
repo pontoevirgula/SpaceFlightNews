3bbf5df8f9cf318fa83161e1c2c608285a788869
carloslima <T3549833@timbrasil.com.br>
2022-11-27 23:00:54 -0300

    master - refatoração do repository

--- app/src/main/java/com/chslcompany/spaceflightnews/data/repository/PostRepositoryImpl.kt
+++ app/src/main/java/com/chslcompany/spaceflightnews/data/repository/PostRepositoryImpl.kt
@@ -2,14 +2,14 @@ package com.chslcompany.spaceflightnews.data.repository
 
 import com.chslcompany.spaceflightnews.core.RemoteException
 import com.chslcompany.spaceflightnews.core.Resource
+import com.chslcompany.spaceflightnews.core.networkBoundResources
 import com.chslcompany.spaceflightnews.data.db.PostDao
 import com.chslcompany.spaceflightnews.data.entities.db.toPostListModel
 import com.chslcompany.spaceflightnews.data.entities.model.Post
+import com.chslcompany.spaceflightnews.data.entities.network.PostDTO
 import com.chslcompany.spaceflightnews.data.entities.network.toPostDbList
 import com.chslcompany.spaceflightnews.data.service.SpaceFlightNewsService
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 
 class PostRepositoryImpl(
@@ -17,24 +17,34 @@ class PostRepositoryImpl(
     private val dao: PostDao
 ) : PostRepository {
 
+    private val readFromDatabase = {
+        dao.listPosts().map {
+            it.sortedByDescending { postDb ->
+                postDb.publishedAt
+            }.toPostListModel()
+        }
+    }
+
+    private val cleanDatabaseAndSave : suspend (List<PostDTO>) -> Unit = { listPostDTO ->
+        dao.clearDb()
+        dao.saveAll(listPostDTO.toPostDbList())
+    }
+
+    private val errorToLoadNews = {
+        RemoteException("Falha ao atualizar as noticias. Exibindo o conteudo antigo...")
+    }
+
     override suspend fun listPosts(category: String): Flow<Resource<List<Post>>> =
         networkBoundResources(
-            query = {
-                dao.listPosts().map {
-                    it.sortedByDescending { postDb ->
-                        postDb.publishedAt
-                    }.toPostListModel()
-                }
-            },
+            query = readFromDatabase,
             service = {
                 service.getListPost(category)
             },
             saveFetchResult = { listPostDTO ->
-                dao.clearDb()
-                dao.saveAll(listPostDTO.toPostDbList())
+                cleanDatabaseAndSave(listPostDTO)
             },
             onError = {
-                RemoteException("Falha ao atualizar as noticias. Exibindo o conteudo antigo...")
+                errorToLoadNews.invoke()
             }
         )
 
@@ -43,61 +53,18 @@ class PostRepositoryImpl(
         titleContains: String?
     ): Flow<Resource<List<Post>>> =
         networkBoundResources(
-            query = {
-                dao.listPosts().map {
-                    it.sortedByDescending { postDb ->
-                        postDb.publishedAt
-                    }.toPostListModel()
-                }
-            },
+            query = readFromDatabase ,
             service = {
                 service.getListPostTitleContains(category, titleContains)
             },
             saveFetchResult = { dtoList ->
-                dao.clearDb()
-                dao.saveAll(dtoList.toPostDbList())
+                cleanDatabaseAndSave(dtoList)
             },
             onError = {
-                RemoteException("Não foi possível carregar lista")
+                errorToLoadNews.invoke()
             }
         )
-
-
-    private inline fun  <ResultType, RequestType> networkBoundResources(
-        crossinline query : () -> Flow<ResultType>,
-        crossinline service : suspend () -> RequestType,
-        crossinline saveFetchResult : suspend (RequestType) -> Unit,
-        crossinline onError : (Throwable) -> Throwable
-    ) : Flow<Resource<ResultType>> =
-        flow {
-            var dbData = query().first()
-            try {
-                if (service() != EMPTY_LIST) {
-                    saveFetchResult(service())
-                    dbData = query().first()
-                    emit(Resource.Success(dbData))
-                } else {
-                    emit(
-                        Resource.Failure(
-                            data = dbData,
-                            error = onError(RemoteException("Lista vazia"))
-                        )
-                    )
-                }
-            } catch (e: Exception) {
-                emit(
-                    Resource.Failure(
-                        data = dbData,
-                        error = onError(e)
-                    )
-                )
-            }
-        }
-
-    companion object{
-        private const val EMPTY_LIST = ""
-    }
-}
+   }
 
 
 
