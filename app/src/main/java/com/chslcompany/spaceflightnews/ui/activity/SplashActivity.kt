a054b31c917c994cff9327f65f6eaf527c585939
carloslima <T3549833@timbrasil.com.br>
2023-01-08 17:46:28 -0300

    master - diminuindo tempo de delay

--- app/src/main/java/com/chslcompany/spaceflightnews/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/chslcompany/spaceflightnews/ui/activity/SplashActivity.kt
@@ -23,6 +23,6 @@ class SplashActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2000)
+        }, 1500)
     }
 }
