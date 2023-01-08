30635bf6d5f7bee1eb350dad7623426e6840fb8e
carloslima <T3549833@timbrasil.com.br>
2023-01-07 23:03:53 -0300

    master - corrigindo animação da splash

--- app/src/main/java/com/chslcompany/spaceflightnews/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/chslcompany/spaceflightnews/ui/activity/SplashActivity.kt
@@ -1,11 +1,13 @@
-package com.chslcompany.spaceflightnews.ui
+package com.chslcompany.spaceflightnews.ui.activity
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.chslcompany.spaceflightnews.R
 
 @SuppressLint("CustomSplashScreen")
@@ -13,6 +15,10 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val backgroundImage: ImageView = findViewById(R.id.rocket_image)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
+        backgroundImage.startAnimation(slideAnimation)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
